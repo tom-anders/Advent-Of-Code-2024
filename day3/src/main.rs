30a0c3b2bbb80837acd286f7ebc37c7372214776
tom-anders <13141438+tom-anders@users.noreply.github.com>
2024-12-03 06:56:12 +0100

    refactor: make day3 pretty

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,32 +1,43 @@
 use aoc_derive::aoc_main;
-use itertools::Itertools;
 use lazy_regex::regex;
 use utils::*;
 
+#[derive(Debug, Copy, Clone)]
+enum Instr {
+    Mul(usize, usize),
+    Do,
+    Dont,
+}
+
 #[aoc_main]
 fn solve(input: Input) -> impl Into<Solution> {
-    let instrs = regex!(r"(do\(\)|don't\(\)|mul\(\d\d?\d?,\d\d?\d?\))")
-        .find_iter(&input.raw)
-        .map(|m| m.as_str())
-        .collect_vec();
-    dbg!(&instrs);
-    let mut sum = 0;
-    let mut sum2 = 0;
-    let mut doo = true;
-    for i in instrs {
-        if i == "do()" {
-            doo = true;
-        } else if i == "don't()" {
-            doo = false;
+    let instrs = (0..input.len()).filter_map(|i| {
+        let s = &input.as_str()[i..];
+        if s.starts_with("do()") {
+            Some(Instr::Do)
+        } else if s.starts_with("don't()") {
+            Some(Instr::Dont)
         } else {
-            let (a, b) = extract_numbers_unsigned(i).collect_tuple().unwrap();
-            sum += a * b;
-            if doo {
-                sum2 += a * b;
-            }
+            regex!(r"^mul\((\d{1,3}),(\d{1,3})\)").captures(s).map(|captures| {
+                Instr::Mul(captures[1].parse().unwrap(), captures[2].parse().unwrap())
+            })
         }
-    }
-    (sum, sum2)
+    });
+
+    let part1 = instrs
+        .clone()
+        .filter_map(|instr| if let Instr::Mul(lhs, rhs) = instr { Some(lhs * rhs) } else { None })
+        .sum_usize();
+
+    let part2 = instrs
+        .fold((0, true), |(sum, doit), instr| match instr {
+            Instr::Mul(lhs, rhs) => (sum + if doit { lhs * rhs } else { 0 }, doit),
+            Instr::Do => (sum, true),
+            Instr::Dont => (sum, false),
+        })
+        .0;
+
+    (part1, part2)
 }
 
 #[cfg(test)]
